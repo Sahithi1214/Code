37536159f8a6cad6959af35c912cc5a62167e8aa
Sahithi1214 <sahithi@tamu.edu>
2025-05-29 17:53:36 -0700

    Problem 20

--- Grind 75/main.java
+++ Grind 75/main.java
@@ -1,9 +1,16 @@
 public class main{
     public static void main(String[] args) {
+        //Problem 1: Two Sum
         // Solution solution = new Solution();
         // int[] nums = {2,7,11,15};
         // int target = 9;
         // int[] result = solution.twoSum(nums, target);
         // System.out.println(Arrays.toString(result));
+
+        //Problem 20: Valid Parentheses
+        // Solution solution = new Solution();
+        // String s = "()";
+        // boolean result = solution.isValid(s);
+        // System.out.println(result);
     }
 }
