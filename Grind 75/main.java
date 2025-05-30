f521400f56320565174d98ec98409127ea2e1c98
Sahithi1214 <sahithi@tamu.edu>
2025-05-29 17:45:13 -0700

    Problem 1

---
+++ Grind 75/main.java
@@ -0,0 +1,9 @@
+public class main{
+    public static void main(String[] args) {
+        // Solution solution = new Solution();
+        // int[] nums = {2,7,11,15};
+        // int target = 9;
+        // int[] result = solution.twoSum(nums, target);
+        // System.out.println(Arrays.toString(result));
+    }
+}
