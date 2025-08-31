66f12edb6250a07fa7badf78302a8616bac1d3cf
Sahithi1214 <sahithi@tamu.edu>
2025-08-31 18:32:28 -0500

    Problem 236

--- Grind 75/main.java
+++ Grind 75/main.java
@@ -148,5 +148,13 @@ public class main{
         // SparseVector v2 = new SparseVector(nums2);
         // int result = v1.dotProduct(v2);
         // System.out.println(result);
+
+        //Problem 236: Lowest Common Ancestor of a Binary Tree
+        // Solution solution = new Solution();
+        // TreeNode root = new TreeNode(3, new TreeNode(5, new TreeNode(6), new TreeNode(2, new TreeNode(7), new TreeNode(4))), new TreeNode(1, new TreeNode(0), new TreeNode(8)));
+        // TreeNode p = new TreeNode(5);
+        // TreeNode q = new TreeNode(1);
+        // TreeNode result = solution.lowestCommonAncestor(root, p, q);
+        // System.out.println(result.val);
     }
 }
